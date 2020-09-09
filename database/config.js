5ef3bd7d9a0390fe328e7fe4dbeb70b227f26c22
Cecilia Garcia <ceciliagarciagon92@gmail.com>
2020-09-09 16:14:31 +0200

    Fin seccion 10- Auth

--- database/config.js
+++ database/config.js
@@ -3,8 +3,14 @@ const mongoose = require('mongoose');
 const dbConnection = async() => {
 
     try {
-        await mongoose.connect(process.env.DB_CNN, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+        await mongoose.connect(process.env.DB_CNN, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
+
         console.log('DB online');
+
     } catch (error) {
         console.log(error);
         throw new Error('Error a la hora de inciar la bd');
--- index.js
+++ index.js
@@ -7,25 +7,23 @@ const { dbConnection } = require('./database/config');
 
 //crear el servidor de express
 const app = express();
+
+
 //configurar cors
-app.use(cors);
+app.use(cors());
+
+//lectura del body
+app.use(express.json());
 
 
 // base de datos
 dbConnection();
 
 
+// Rutas
+app.use('/api/usuarios', require('./routes/usuarios'));
+app.use('/api/login', require('./routes/auth'));
 
-
-//Rutas
-app.get('/', (req, res) => {
-
-    res.json({
-        ok: true,
-        msg: "Hola Mundo"
-    })
-
-});
 
 app.listen(process.env.PORT, () => {
     console.log('Servidor corriendo en el puerto ' + process.env.PORT);
